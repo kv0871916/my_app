5ac91f331445df483cccb011a0a9712fb7290c08
krishna vishwakarma <60446420+kv0871916@users.noreply.github.com>
2022-03-31 10:20:26 +0530

    Windows Platform working now

--- lib/setting/views/body/setting_body.dart
+++ lib/setting/views/body/setting_body.dart
@@ -3,7 +3,9 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_windows/webview_windows.dart';
 
 class CustomBody extends StatefulWidget {
   const CustomBody({
@@ -19,8 +21,8 @@ class CustomBody extends StatefulWidget {
 }
 
 class _CustomBodyState extends State<CustomBody> {
-  final Completer<WebViewController> _controller =
-      Completer<WebViewController>();
+  final _controller = Completer<WebViewController>();
+  final _windowscontroller = WebviewController();
 
   @override
   void initState() {
@@ -28,27 +30,80 @@ class _CustomBodyState extends State<CustomBody> {
     if (Platform.isAndroid) {
       WebView.platform = SurfaceAndroidWebView();
     }
+    if (Platform.isWindows) {
+      initPlatformState();
+    }
+  }
+
+  Future<void> initPlatformState() async {
+    // Optionally initialize the webview environment using
+    // a custom user data directory
+    // and/or a custom browser executable directory
+    // and/or custom chromium command line flags
+    //await WebviewController.initializeEnvironment(
+    //    additionalArguments: '--show-fps-counter');
+
+    try {
+      await _windowscontroller.initialize();
+
+      await _windowscontroller.setBackgroundColor(Colors.transparent);
+      await _windowscontroller
+          .setPopupWindowPolicy(WebviewPopupWindowPolicy.deny);
+      await _windowscontroller.loadUrl(widget.url);
+
+      if (!mounted) return;
+      setState(() {});
+    } on PlatformException catch (e) {
+      WidgetsBinding.instance?.addPostFrameCallback((_) {
+        showDialog<void>(
+            context: context,
+            builder: (_) => AlertDialog(
+                  title: const Text('Error'),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Code: ${e.code}'),
+                      Text('Message: ${e.message}'),
+                    ],
+                  ),
+                  actions: [
+                    TextButton(
+                      child: Text('Continue'),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    )
+                  ],
+                ));
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: WebView(
-        onWebViewCreated: _controller.complete,
-        javascriptMode: JavascriptMode.unrestricted,
-        onProgress: (int progress) {
-          if (kDebugMode) print('WebView is loading (progress : $progress%)');
-        },
-        onPageStarted: (String url) {
-          if (kDebugMode) print('Page started loading: $url');
-        },
-        onPageFinished: (String url) {
-          if (kDebugMode) print('Page finished loading: $url');
-        },
-        gestureNavigationEnabled: true,
-        backgroundColor: const Color(0x00000000),
-        initialUrl: widget.url,
-      ),
+      child: Platform.isWindows
+          ? Webview(
+              _windowscontroller,
+            )
+          : WebView(
+              onWebViewCreated: _controller.complete,
+              javascriptMode: JavascriptMode.unrestricted,
+              onProgress: (int progress) {
+                if (kDebugMode)
+                  print('WebView is loading (progress : $progress%)');
+              },
+              onPageStarted: (String url) {
+                if (kDebugMode) print('Page started loading: $url');
+              },
+              onPageFinished: (String url) {
+                if (kDebugMode) print('Page finished loading: $url');
+              },
+              gestureNavigationEnabled: true,
+              backgroundColor: const Color(0x00000000),
+              initialUrl: widget.url,
+            ),
     );
   }
 }
